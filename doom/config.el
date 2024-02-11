8b283db64fe32fd98f21fa25ced447147476d72c
Manmohan Shukla <manmshuk@gmail.com>
2024-02-11 13:28:36 +0530

    mon settings update

--- doom/config.el
+++ doom/config.el
@@ -20,8 +20,9 @@
 ;;
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
-(setq doom-font (font-spec :family "Fira Code" :size 13)
-      doom-variable-pitch-font (font-spec :family "Fira Code" :size 14))
+;;
+(setq doom-font (font-spec :family "Monospace" :size 10 :weight 'normal))
+;;      doom-variable-pitch-font (font-spec :family "Monospace" :size 10))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
@@ -122,3 +123,4 @@
   '((tab-mark 9 [124 9] [92 9]))) ; 124 is the ascii ID for '\|'
 (global-whitespace-mode) ; Enable whitespace mode everywhere
 ; END TABS CONFIG
+(global-visual-line-mode t)
